1359060f936eb9d25a7dd241ae78f6de81d530ed
Phillip Dequina <pdequina@umich.edu>
2023-05-24 13:50:35 -0400

    [ Easy ] | Acceptance Rate 74.0%

---
+++ 0104 | Maximum Depth of Binary Tree/0104-maximum-depth-of-binary-tree.py
@@ -0,0 +1,17 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def maxDepth(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        if not root:
+            return 0
+        left = 1 + self.maxDepth(root.left)
+        right = 1 + self.maxDepth(root.right)
+        return max(left, right)
