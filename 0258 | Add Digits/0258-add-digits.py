3007d2872188d2d12cb7f4bc0727805f314aa8d3
Phillip Dequina <pdequina@umich.edu>
2023-05-28 16:37:09 -0400

    [ Easy ] | Acceptance Rate 65.7%
    Time: 12 ms (95.87%) | Space: 13.3 MB (77.89%)
    https://leetcode.com/problems/add-digits/submissions/959199527/

---
+++ 0258 | Add Digits/0258-add-digits.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def addDigits(self, num):
+        """
+        :type num: int
+        :rtype: int
+        """
+        if num == 0: return 0
+        return num % 9 if num % 9 else 9
